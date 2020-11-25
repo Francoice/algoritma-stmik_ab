2fc00d1e8d290db1b2098a48a485039dd741fafd
Francoice <fantasticfour.labatubo@gmail.com>
2020-11-25 18:17:28 +0700

    belajar

---
+++ latihan.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int A[5];
+    printf("\n%X", &A[0]);
+    printf("\n%X", &A[1]);
+    printf("\n%X", &A[2]);
+    printf("\n%X", &A[3]);
+    printf("\n%X", &A[4]);
+}
